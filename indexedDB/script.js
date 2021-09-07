92bb466a5870e17116d1bcf2e289cad982fdf4d2
Nikhil Shinde <nikhilshinde00@outlook.com>
2021-09-07 18:58:50 +0530

    indexedDB opening and using transaction

--- indexedDB/script.js
+++ indexedDB/script.js
@@ -1,26 +1,70 @@
+let openBtn = document.querySelector("#open");
+let addBtn = document.querySelector("#add");
+let input = document.querySelector("input");
+let db;
 
-//database version signifies how many major changes my database has been through
-let req = indexedDB.open("Notes",1);
 
-
-req.addEventListener("success",function()
+addBtn.addEventListener("click",function(e)
 {
-    let db = req.result;
-    console.log(1);
+    if(!db)
+    {
+         alert("Database has not been opened yet");
+         return ;
+    }
+
+
+    let value = input.value;
+    input.value = "";
+
+
+
+    //now when we have to do any task that is read,store,update,insert or delete we have to use TRANSACTION
+    // Transaction is just a process of doing something with database
+    //Transaction tell which type of task we have to do that is readonly or readwrite
+
+
+    let tx = db.transaction("csNotes","readwrite");
+    //this give transaction of csNotes that we want to perform some task. That task can be insert,update, delete.
+
+    let csNotesObjectStore = tx.objectStore("csNotes");
+    //this give the access of the csNotes
+    
+
+    let data = {
+        cId:Date.now(),
+        //this give current data time in second
+        note:value
+    }
+    csNotesObjectStore.add(data);
+    //to add data data in the csNotes Object storage 
+
+})
+openBtn.addEventListener("click",function()
+{
+    let req = indexedDB.open("Notes",1);
+
+    req.addEventListener("success",function()
+    {
+        db = req.result;
+        alert("Successfully Opened");
+    })
+    
+    req.addEventListener("upgradeneeded",function()
+    {
+        db = req.result;
+        db.createObjectStore("csNotes",{keyPath:"cId"});
+    })
+    
+    req.addEventListener("error",function()
+    {
+        alert("Error in the opening database");
+    })
 })
 
-//kuch bhi creation ya fir update ka kaam ho to ye wala apka event chlta h
-req.addEventListener("upgradeneeded",function()
-{
-    let db = req.result;
-    console.log(db);
-    console.log(2);
 
-    // when we want to introduce a new object stores then we change the version of the database
-    db.createObjectStore("csNotes",{keyPath:"cId"});
-})
 
-req.addEventListener("success",function()
-{
-    console.log(3);
-})
+
+
+
+
+
