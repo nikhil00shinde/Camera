922e4b02578bc7be82315e494102669a90ee3e21
Nikhil Shinde <nikhilshinde00@outlook.com>
2021-09-07 17:44:19 +0530

    implement the note section

--- indexedDB/script.js
+++ indexedDB/script.js
@@ -1,28 +1,23 @@
-// we are going to use indexedDB, it is a NoSQL database
-//SQL is a language which uses platform like MySQL to perform RDBMS
-
-
-// .open("database_name","database_version") -> it is a asynchronous function
-// Not every asynchronous function use a promise
 
 //database version signifies how many major changes my database has been through
 let req = indexedDB.open("Notes",1);
 
-//req is request, it has 3 event success, error and upgradeneeded
 
 req.addEventListener("success",function()
 {
-    // if the request has been successful request gives result which is database
     let db = req.result;
     console.log(1);
 })
 
+//kuch bhi creation ya fir update ka kaam ho to ye wala apka event chlta h
 req.addEventListener("upgradeneeded",function()
 {
-    // if the request has been successful request gives result which is database
     let db = req.result;
     console.log(db);
     console.log(2);
+
+    // when we want to introduce a new object stores then we change the version of the database
+    db.createObjectStore("csNotes",{keyPath:"cId"});
 })
 
 req.addEventListener("success",function()
